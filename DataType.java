67bc09e8952f4cbb8fe2ff5c89d17f9147dd69e1
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-08-20 09:41:09 -0400

    aug_20

---
+++ DataType.java
@@ -0,0 +1,17 @@
+public class DataType{
+    public static void main(String[] args){
+        System.out.println("hi");
+
+        // you can initialize without assigning like JS
+        int radius;
+        radius = 20;
+        System.out.println(radius);
+        // literal = actual value
+            // literal of radius = 20
+            
+        double newRadius = 20.0;
+        System.out.println(newRadius);
+
+        // dollar sign identifiers
+    }
+}
