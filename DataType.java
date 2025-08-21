9c573e58a1ab17b1ab96916800d4b1fccd023d80
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-08-21 10:05:04 -0400

    AUG_21

--- DataType.java
+++ DataType.java
@@ -1,6 +1,13 @@
+
+import java.util.*;
+
+
 public class DataType{
     public static void main(String[] args){
-        System.out.println("hi");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your name");
+        double input = keyboard.nextDouble();
+        System.out.println("hi "+input);
 
         // you can initialize without assigning like JS
         int radius;
