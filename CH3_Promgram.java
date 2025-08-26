642f26acf3585ce8b26b3814209f96073dc6cd48
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-08-26 09:54:04 -0400

    added commits

--- CH3_Promgram.java
+++ CH3_Promgram.java
@@ -1,15 +1,19 @@
 /*
  Name: Angel Carmichael
- Class:
-
+ Class: Period 2
+ Date: 8/26/25
  */
 
 import java.util.*;
 public class CH3_Promgram {
     public static void main(String[] args) {
+        /* grab the keyboard scanner */
         Scanner val = new Scanner(System.in);
+
+/* define root veariable */
 double root1 = 0.0;
 double root2 = 0.0;
+
         /* obtain the value of a,b,c: */
         System.out.println("What is the value of A:");
         double a = val.nextDouble();
@@ -20,7 +24,7 @@ double root2 = 0.0;
         System.out.println("What is the value of C:");
         double c = val.nextDouble();
 
-        /* finding the value of discriminate */
+        /* out put based on the value of discriminate */
         if (Math.pow(b,2)-(4*a*c)>0){
             root1 = (-b-Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
             root2 = (-b+Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
@@ -28,8 +32,8 @@ double root2 = 0.0;
         } else if (Math.pow(b,2)-(4*a*c)<0){
             System.out.println("The equation has no real roots.");
         } else{
-            double root = (-b+Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
-            System.out.println(root + " is your root");
+            root1 = (-b+Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
+            System.out.println(root1 + " is your root");
         }
-        }
+    }
 }
