9a7f46c23f673996d1e9628a7d063f624640af89
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-09-08 10:18:58 -0400

    9/8/25

--- Carmichael_1.java
+++ Carmichael_1.java
@@ -2,7 +2,7 @@
 Name: Angel Carmichael
 Class: Period 2
 Created: 9/5/25
-Last Modified:
+Last Modified: 9/8/2025
 Purpose: Write a java program that wil calculate ticket for 3 customers based on their ages. 
 */
 
@@ -10,57 +10,84 @@ Purpose: Write a java program that wil calculate ticket for 3 customers based on
 import java.util.Scanner;
 
 public class Carmichael_1 {
-    public static String ageVerification(int a) {
+
+    /* Function for finding the price based on age range*/
+    public static double ageVerification(int a) {
+        double price = 0.0;
         if (a<0){
-            return "You can't be a negative age?...";
         } else if (a>=0 && a<=5){
-            return "";
+            price = 0.0;
         } else if (a>5 && a<=12) {
-            return "";
+            price = (5+(5*0.05));
         } else if (a>12 && a<=17) {
-            return "";
+            price = (8+(8*0.05));
         } else if (a>17 && a<=59) {
-            return "";
+            price = (12+(12*0.05));
         } else if (a>59) {
-            return "";
+            price = (6+(6*0.05));
         }
+        return price;
     }
     
     public static void main(String[] args) {
-        String name = " ";
-        int age = 0;
-
         Scanner keyboard = new Scanner(System.in);
+        /* Declaring my veriables */
+        String name1 = " ";
+        String name2 = " ";
+        String name3 = " ";
 
-        
+        int age1 = 0;
+        int age2= 0;
+        int age3 = 0;
 
+        double price1 = 0.0;
+        double price2 = 0.0;
+        double price3 = 0.0;
+
+        /* Getting customer 1 input */
         System.out.println("Welcome to the theater of Mindscape Cinema!!!!");
-        System.out.println("What is the first customers Last name?");
-        name = keyboard.next();
-        System.out.print("What is the age of this customer?:");
-        age = keyboard.nextInt();
+        System.out.print("What is the first customers Last name?: ");
+        name1 = keyboard.next();
+        System.out.print("What is the age of this customer?: ");
+        age1 = keyboard.nextInt();
 
-        System.out.println((name.toLowerCase() == "angel") ? "OMGGGGG!!!!, MY NAME IS ANGEL." : "Cool name");
+        /* calls the users ageVerfuction function  to get the price */
+        price1 = ageVerification(age1);
         
-        
+        System.out.println();
+
+        /* Getting customer 2 input */
+        System.out.print("What is the last name of the second customer?: ");
+        name2 = keyboard.next();
+        System.out.print("What is the age of this customer?: ");
+        age2 = keyboard.nextInt();
+
+        /* calls the users ageVerfuction function  to get the price */
+        price2 = ageVerification(age2);
 
         System.out.println();
 
-        System.out.print("What is the name of the second customer?: ");
-        name = keyboard.next();
+        /* Getting customer 3 input */
+        System.out.print("What is the last name of the third customer?: ");
+        name3 = keyboard.next();
         System.out.print("What is the age of this customer?: ");
-        age = keyboard.nextInt();
+        age3 = keyboard.nextInt();
 
-        System.out.println((name.toLowerCase() == "angel") ? "OMGGGGG!!!!, MY NAME IS ANGEL." : "Cool name");
+        /* calls the users ageVerfuction function  to get the price */
+        price3 = ageVerification(age3);
 
+        /* output customer output */
+        System.out.println("Customer 1 Name: " + name1 + "\nCustomer 1 Age: " + age1 + "\nCustomer 1 Ticket Price: $" + price1+"\n");
+        System.out.println("Customer 2 Name: " + name2 + "\nCustomer 2 Age: " + age2 + "\nCustomer 2 Ticket Price: $" + price2+"\n");
+        System.out.println("Customer 3 Name: " + name3 + "\nCustomer 3 Age: " + age3 + "\nCustomer 3 Ticket Price: $" + price3+"\n");
 
-        System.out.print("What is the name of the third customer?: ");
-        name = keyboard.next();
-        System.out.print("What is the age of this customer?: ");
-        age = keyboard.nextInt();
-
-        System.out.println((name.toLowerCase() == "angel") ? "OMGGGGG!!!!, MY NAME IS ANGEL." : "Cool name");
-
+        System.out.println("______________________Calculations______________________\n");
         
+        /* customer total calculations */
+        System.out.print("Subtotal: ");
+        System.out.println((int)price1+(int)price2+(int)price3);
+
+        System.out.print("Total Charge: ");
+        System.out.println((float)price1+price2+price3);
     }
 }
