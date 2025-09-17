580549dc9f4162f290c26b62137aeb0afb9f0c15
Angel Carmichael <amcarmichael2@my.waketech.edu>
2025-09-17 09:34:02 -0400

    9/17/25

--- ADCT.java
+++ ADCT.java
@@ -1,27 +1,47 @@
+/*
+Name: Angel Carmichael
+Class: Period 2
+Created: 9/16/25
+Last Modified: 9/17/2025
+Purpose: Write a method that computes future investment value at a given interest rate for a specified number of years. 
+*/
 
 import java.util.Scanner;
 
 public class ADCT {
+    /* make an function to the formula of finding the future investment value */
     public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
         double futureInvest = 0.0;
-        futureInvest = investmentAmount*(1+monthlyInterestRate)*years*12;
+        futureInvest = investmentAmount*Math.pow((1+monthlyInterestRate),years*12);
         return futureInvest;
     }
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        double investmentRate = keyboard.nextDouble();
-        double loan = keyboard.nextDouble();
-        int year = keyboard.nextInt();
 
-        double futurevalue = futureInvestmentValue(loan, investmentRate, year);
+    /* main function */
+    public static void main(String[] args) {
+        /* initialize at the top */
+        double futurevalue = 0.0;
+
+        /* creat scanner key and variables */
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter you invest rate: ");
+        double investmentRate = keyboard.nextDouble();
+
+        System.out.print("Enter you investment amount: ");
+        double loan = keyboard.nextDouble();
+
+        /* formating fo the loan investment output */
+        System.out.println();
         System.out.println("The amount investment: $"+ loan);
         System.out.println("_________________________________");
-        System.out.println("Amount interest Rate: $" + investmentRate);
+        System.out.println("Amount interest Rate: " + investmentRate);
         System.out.println("________________________________________");
-        for (Object elem : col) {
-            System.out.println("Years: "+ i);
-        }
-        
+        System.out.println();
 
+        /* loops through 30 years and find the investment value by that year */
+        for (int i = 1; i<=30; i++) {
+            System.out.println("Years: "+ i);
+            futurevalue = futureInvestmentValue(loan, investmentRate/12, i);
+            System.out.println("Future Value: " + futurevalue);
+        }
     }
 }
